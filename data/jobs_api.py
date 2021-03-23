8e66161b10e6dbdaa50ac6052d8e1dbbbe112e65
I-love-you-contest <maxim.valo@yandex.ru>
2021-03-23 20:19:39 +0500

    Task 3

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -11,7 +11,7 @@ blueprint = Blueprint(
 )
 
 
-@blueprint.route('/api/jobs')
+@blueprint.route('/api/jobs', methods=['GET'])
 def get_all():
     sess = db_session.create_session()
     jobs = sess.query(Job).all()
@@ -25,3 +25,20 @@ def get_one(job_id):
     if not job:
         return jsonify({'ERROR': 'Job not found'}), 404
     return jsonify({job.id: job.name})
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create():
+    args = request.json
+    if not args:
+        return jsonify({"ERROR": "EMPTY REQUEST"}), 400
+    elif 'name' not in args:
+        return jsonify({"ERROR": "BAD REQUEST. NAME IS MISSING"}), 400
+    elif set(args.keys()) > {'name', 'id'}:
+        return jsonify({"ERROR": "BAD REQUEST. UNKNOWN ARGUMENTS"}), 400
+    sess = db_session.create_session()
+    job = Job(**args)
+    sess.add(job)
+    sess.commit()
+    return jsonify({'success': {'id': job.id}})
+
