5bc7f7df9c49786dc6a74a6de2a5c671c4b21a19
I-love-you-contest <maxim.valo@yandex.ru>
2021-03-23 17:59:29 +0500

    Task 1

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,5 +1,5 @@
 """Полный код для теста на https://github.com/I-love-you-contest/jobs-api"""
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 
 from . import db_session
 from .jobs import Job
@@ -16,3 +16,12 @@ def get_all():
     sess = db_session.create_session()
     jobs = sess.query(Job).all()
     return jsonify({x.id: x.name for x in jobs})
+
+
+@blueprint.route('/api/jobs/<int:job_id>')
+def get_one(job_id):
+    sess = db_session.create_session()
+    job = sess.query(Job).get(job_id)
+    if not job:
+        return jsonify({'ERROR': 'Job not found'}), 404
+    return jsonify({job.id: job.name})
